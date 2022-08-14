fa38789ced9339e52b9e4b0f693de665991351f8
Timovanmilligen <57662782+Timovanmilligen@users.noreply.github.com>
2022-08-14 13:02:54 +0200

    Update plots

--- plot.R
+++ plot.R
@@ -1,14 +1,20 @@
 require(tidyverse)
 require(reshape2)
-setwd("~/opendc2/Results")
+setwd("~/GitHub/opendc")
 #Load data
 FF <- read.table("New results/Baseline/FirstFit.txt",header = TRUE)
 LCL <- read.table("New results/Baseline/LowestCpuLoad.txt",header = TRUE)
 LML <- read.table("New results/Baseline/LowestMemoryLoad.txt",header = TRUE)
 MCL <- read.table("New results/Baseline/MaximumConsolidationLoad.txt",header = TRUE)
 VCPU <- read.table("New results/Baseline/VCpuCapacity.txt",header = TRUE)
-PS <- read.table("New results/Baseline/Portfolio_Scheduler20m.txt",header = TRUE)
-PSHistory <- read.table("New results/Baseline/Portfolio_Scheduler20m_history.txt")
+PS20 <- read.table("New results/Baseline/Portfolio_Scheduler20m.txt",header = TRUE)
+PS20History <- read.table("New results/Baseline/Portfolio_Scheduler20m_history.txt")
+
+PS40 <- read.table("New results/Baseline/Portfolio_Scheduler40m.txt",header = TRUE)
+PS40History <- read.table("New results/Baseline/Portfolio_Scheduler40m_history.txt")
+
+PS60 <- read.table("New results/Baseline/Portfolio_Scheduler60m.txt",header = TRUE)
+PS60History <- read.table("New results/Baseline/Portfolio_Scheduler60m_history.txt")
 
 PSExtended <- read.table("New results/Extended/Portfolio_Scheduler20m.txt",header = TRUE)
 PSExtendedHistory <- read.table("New results/Extended/Portfolio_Scheduler20m_history.txt")
@@ -17,7 +23,9 @@ PSExtended2 <- read.table("New results/Extended2/Portfolio_Scheduler20m.txt",hea
 PSExtended2History <- read.table("New results/Extended2/Portfolio_Scheduler20m_history.txt")
 #Add data ----------------------------------------
 FF$cumulative_cpu_usage <- cumsum(FF$cpu_usage)
-PS$cumulative_cpu_usage <- cumsum(PS$cpu_usage)
+PS20$cumulative_cpu_usage <- cumsum(PS20$cpu_usage)
+PS40$cumulative_cpu_usage <- cumsum(PS40$cpu_usage)
+PS60$cumulative_cpu_usage <- cumsum(PS60$cpu_usage)
 LCL$cumulative_cpu_usage <- cumsum(LCL$cpu_usage)
 LML$cumulative_cpu_usage <- cumsum(LML$cpu_usage)
 MCL$cumulative_cpu_usage <- cumsum(MCL$cpu_usage)
@@ -32,8 +40,12 @@ LML$Overprovisioned <- LML$cpu_demand/LML$cpu_usage
 
 #Combine data in data frame
 combined_data <- FF %>%  mutate(Type = 'First Fit') %>%
-  bind_rows(PS %>%
-              mutate(Type = 'PS')) %>% 
+  bind_rows(PS20 %>%
+              mutate(Type = 'PS 20m')) %>% 
+  bind_rows(PS40 %>%
+              mutate(Type = 'PS 40m')) %>% 
+  bind_rows(PS60 %>%
+              mutate(Type = 'PS 60m')) %>% 
   bind_rows(LCL %>% 
               mutate(Type = "LCL")) %>% 
   bind_rows(MCL %>% 
@@ -78,7 +90,7 @@ ggplot(combined_data,aes(y = cpu_demand,x = Time_minutes,color = Type)) +
 #CPU demand
 ggplot() + 
   geom_line(data=FF, aes(x=Time_minutes, y = cpu_usage),color = "green", alpha = 0.5)+
-  geom_line(data=PS, aes(x=Time_minutes, y = cpu_usage),color = "blue", alpha = 0.5)+
+  geom_line(data=PS20, aes(x=Time_minutes, y = cpu_usage),color = "blue", alpha = 0.5)+
   ggtitle("Cpu demand MHz")  
 
 #CPU Idle
@@ -102,15 +114,23 @@ ggplot(combined_data,aes(y = Overprovisioned,x = Time_minutes,color = Type)) +
   ggtitle("Overprovisioned ratio (cpu demand / cpu usage")
 
 #Cpu demand and usage in one
-df <- melt(PS[c("Time_minutes","cpu_demand","cpu_usage")] ,  id.vars = 'Time_minutes', variable.name = 'Variable')
+df <- melt(PS20[c("Time_minutes","cpu_demand","cpu_usage")] ,  id.vars = 'Time_minutes', variable.name = 'Variable')
 #create line plot for each column in data frame
 ggplot(df, aes(Time_minutes, value)) +
   geom_line(aes(colour = Variable)) +
   ggtitle("Cpu demand and usage (MHz) Portfolio Scheduler")
 
-
+#Change active scheduler names --------------------------
+PS20History$V2 <- replace(PS20History$V2, PS20History$V2 == "Weighers:RamWeigher[multiplier=1.0],Filters:ComputeFilter-VCpuFilter[allocationRatio=16.0]-RamFilter[allocationRatio=1.0]subsetSize:1",
+                          "LML")
+PS20History$V2 <- replace(PS20History$V2, PS20History$V2 == "Weighers:MaximumConsolidationLoad[multiplier=1.0],Filters:ComputeFilter-VCpuFilter[allocationRatio=16.0]-RamFilter[allocationRatio=1.0]subsetSize:1",
+                          "MCL")
+PS20History$V2 <- replace(PS20History$V2, PS20History$V2 == "Weighers:FFWeigher[multiplier=1.0],Filters:ComputeFilter-VCpuFilter[allocationRatio=16.0]-RamFilter[allocationRatio=1.0]subsetSize:1",
+                          "FF")
+PS20History$V2 <- replace(PS20History$V2, PS20History$V2 == "Weighers:CpuLoadWeigher[multiplier=-1.0],Filters:ComputeFilter-VCpuFilter[allocationRatio=16.0]-RamFilter[allocationRatio=1.0]subsetSize:1",
+                          "LCL")
 #Active scheduler plots
-ggplot(PSHistory,aes(V1,V2,group = 1)) +
+ggplot(PS20History,aes(V1,V2,group = 1)) +
   geom_point() + geom_line()
 
 ggplot(PSExtendedHistory,aes(V1,V2,group = 1)) +
